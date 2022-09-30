0021211613a15f3934fc8ac1be6a194eaba5bb80
Евгения Ольшанова <olshanova1995@gmail.com>
2022-09-30 15:23:18 +0300

    add files in brain-games

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import random
+from random import randint
+import prompt
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    count = 0
+    while count < 3:
+      question = random.randint(1, 100)
+      print(f'Question: {question}')
+      your_answer = prompt.string('Your answer: ')
+
+      if question % 2 == 0:
+        true_answer = 'yes'
+      else:
+        true_answer = 'no'
+  
+      if question % 2 == 0 and your_answer == 'yes':
+        count += 1
+        print('Correct!')
+      elif question % 2 != 0 and your_answer == 'no':
+        count += 1
+        print('Correct!')
+      elif question % 2 == 0 and your_answer == 'no':
+        print(f'\'{your_answer}\' is wrong answer ;(. Correct answer was \'{true_answer}\'.\nLet\'s try again, {name}!')
+        break
+      elif question % 2 != 0 and your_answer == 'yes':
+        print(f'\'{your_answer}\' is wrong answer ;(. Correct answer was \'{true_answer}\'.\nLet\'s try again, {name}!')
+        break
+      elif your_answer != 'yes' and your_answer != 'no':
+        print(f'\'{your_answer}\' is wrong answer ;(. Correct answer was \'{true_answer}\'.\nLet\'s try again, {name}!')
+        break
+
+    if count == 3:
+      print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
