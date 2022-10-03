7f6fecb79a52881d082c07140f6a9278e9e21ca6
Евгения Ольшанова <olshanova1995@gmail.com>
2022-10-03 20:12:30 +0300

    переписала gcd.ru - новые названия

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,8 +5,8 @@ game_task = 'Find the greatest common divisor of given numbers.'
 
 
 def generate_round():
-    x = random.randint(1, 26)
-    y = random.randint(26, 48)
-    goal = f'{x} {y}'
-    result = gcd(x, y)
+    num1 = random.randint(1, 26)
+    num2 = random.randint(26, 48)
+    goal = f'{num1} {num2}'
+    result = gcd(num1, num2)
     return str(goal), str(result)
