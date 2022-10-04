90d0128f07cdaa43631d121e7ec84bf87a0cdf93
Евгения Ольшанова <olshanova1995@gmail.com>
2022-10-04 21:42:21 +0300

    Исправила ошибку теста в prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,6 +7,7 @@ game_task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def generate_round():
     prime_number = random.randint(0, 129)
     diapason = range(2, prime_number)
+    result = ''
     if prime_number == 0 and prime_number == 1:
         result = 'no'
     for elem in diapason:
@@ -15,4 +16,4 @@ def generate_round():
             break
         result = 'yes'
     goal = prime_number
-    return str(goal), result
+    return str(goal), str(result)
