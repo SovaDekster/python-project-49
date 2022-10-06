0e0bbb8df34984d44f241b45fef1bd83718fb697
Евгения Ольшанова <olshanova1995@gmail.com>
2022-10-06 15:48:58 +0300

    Переписала игру на прогрессию

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,21 +3,23 @@ import random
 
 RULE = 'What number is missing in the progression?'
 
+first_number = random.randint(0, 34)
+length = random.randint(5, 10)
+step = random.randint(1, 8)
+
+
+def generate_progression():
+    progression = [str(first_number), ]
+    current_step = 1
+    current_number = first_number
+    while current_step <= length:
+        current_number += step
+        progression.append(str(current_number))
+        current_step += 1
+    return progression
+
 
 def generate_round():
-    first_number = random.randint(0, 34)
-    length = random.randint(5, 10)
-    step = random.randint(1, 8)
-
-    def generate_progression():
-        progression = [str(first_number), ]
-        current_step = 1
-        current_number = first_number
-        while current_step <= length:
-            current_number += step
-            progression.append(str(current_number))
-            current_step += 1
-        return progression
     progression = generate_progression()
     random_index = random.randint(0, (len(progression) - 1))
     correct_answer = str(progression[random_index])
