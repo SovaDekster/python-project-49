49f5a769f0bac99503df1a7840acbbb7d790d160
Евгения Ольшанова <olshanova1995@gmail.com>
2022-10-06 19:11:51 +0300

    Исправила prime.py - ошибка вычислений

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,11 +1,12 @@
 import random
+import math
 
 
 RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    diapason = range(2, number)
+    diapason = range(2, int(math.sqrt(number)) + 1)
     if number <= 1:
         return False
     for elem in diapason:
